a01da597aa0a1b39c9094c543c8da8d816e15d25
southf09 <frankstonesouth@gmail.com>
2021-08-30 11:36:30 -0400

    Added basic CSS, and imported 'Roboto' font from Google Fonts

---
+++ app.js
@@ -0,0 +1,12 @@
+//Array that will hold the library
+let myLibrary = [];
+
+//Book constructor
+function book() {
+    this.name
+}
+
+//Function that adds the book to the library
+function addBookToLibrary() {
+
+}
